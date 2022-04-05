8bf7bc2b281c29174d8fee728e5b873672025247
André Carlos Jr <andre-carlos@live.com>
2022-04-04 22:39:28 -0300

    - add types

---
+++ types/index.ts
@@ -0,0 +1,35 @@
+export interface User {
+	provider: "steam";
+	_json: {
+		steamid: "76561198287998371";
+		communityvisibilitystate: 3;
+		profilestate: 1;
+		personaname: "Eroshi";
+		profileurl: "https://steamcommunity.com/id/andrecrjr/";
+		avatar: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/1c/1c8beed647f8688556838627501d72612b24be3b.jpg";
+		avatarmedium: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/1c/1c8beed647f8688556838627501d72612b24be3b_medium.jpg";
+		avatarfull: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/1c/1c8beed647f8688556838627501d72612b24be3b_full.jpg";
+		avatarhash: "1c8beed647f8688556838627501d72612b24be3b";
+		lastlogoff: 1647054874;
+		personastate: 1;
+		realname: "André carlos Jr.";
+		primaryclanid: "103582791429521408";
+		timecreated: 1456868056;
+		personastateflags: 0;
+		loccountrycode: "BR";
+		locstatecode: "21";
+	};
+	id: "76561198287998371";
+	displayName: "Eroshi";
+	photos: [
+		{
+			value: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/1c/1c8beed647f8688556838627501d72612b24be3b.jpg";
+		},
+		{
+			value: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/1c/1c8beed647f8688556838627501d72612b24be3b_medium.jpg";
+		},
+		{
+			value: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/1c/1c8beed647f8688556838627501d72612b24be3b_full.jpg";
+		}
+	];
+}
